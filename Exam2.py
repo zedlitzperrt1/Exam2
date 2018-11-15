1b4194d7bf7e768e9fb2c87b8fe54d07edc21d98
zedlitzperrt1 <45074025+zedlitzperrt1@users.noreply.github.com>
2018-11-15 12:08:11 -0600

    Update Exam2.py

--- Exam2.py
+++ Exam2.py
@@ -1,11 +1,11 @@
 """ Due end of class Thursday
-
+/
 BONUS EXAM 1
 
 NAMES: 
     Paul Davis
     Gabby Lazo
-    Tiffany Harry
+    Tiffany Perry
     Daniel Gomez
     
 Write pseudo code in your script, then list all solutions in Python
@@ -140,6 +140,85 @@ def number8(self):
     return k
 
 """
+16.  Describe an algorithm for finding the smallest integer in a finite sequence of natural numbers.
+
+Make an array of natural numbers
+Store the first element in the array as a temp variable.
+Begin a loop that starts at the first element and continues to the last element.
+If the current element is smaller than the temp variable, swap them.
+
+Output: The smallest variable
+"""
+17. Describe an algorithm that locates the first occurrence of the largest element in a finite list of integers, 
+where the intgers in the list are not necessrily distinct.
+
+Make an array of ints
+Store the first element in the array as a temp variable.
+Begin a loop that starts at the first element and continues to the last element.
+If the current element is larger than the temp variable, swap them.
+Cycle through the array again from the first element, if element=temp, return location of element.
+
+Output: Element of first occurence of smallest number.
+"""
+
+18. Descrie an algorithm that locates the last occurence of the smallest element in a finite list of ints, where 
+the ints are not necessarily distinct.
+
+Make an array of ints
+Store the first element in the array as a temp variable.
+Begin a loop that starts at the first element and continues to the last element.
+If the current element is smaller than the temp variable, swap them.
+Cycle through the array again from the first element, if element=temp, store location of element in seperate list.
+Sort the list of locations.
+The last occurence equals the largest location number.
+
+Output: Element of last occurence of smallest number.
+"""
+
+19. Describe an algorithm that produces the max, median, mean ,and min of a set of 3 ints.
+
+Make an array of 3 integers 
+
+1.Sort the array
+Begin a loop that starts at the first element.
+If the current element is smaller than previous element, swap them. 
+Loop through entire array until the number of swaps is 0.
+
+2.Find the min
+Min equals the first element.
+
+3.Find the max
+Max equals the last element.
+
+4.Find the median
+Divide the largest element number by two (in this case, three)
+(Return the data stored in the given element.)
+
+5.Find the mean
+Add all of the data in the elements once.
+Divide by two.
+
+
+Output: min,max, median, mean, min.
+"""
+
+20. Describe an algorithm for finding both the largest and the smallest integers in a finite
+sequence of integers.
+
+Make an array of integers
+
+1.Sort the array
+Begin a loop that starts at the first element.
+If the current element is smaller than previous element, swap them.
+Loop through entire array until the number of swaps is 0.
+
+2.Find the min
+Min equals the first element.
+
+3.Find the max
+Max equals the last element.
+
+
 31. Devise an algorithm that finds the first term of a sequence
 of integers that equals some previous term in the
 sequence.
@@ -206,4 +285,15 @@ def number33(self):
     for i in range(1, len(self)):
         if self[i] < self[i-1]:
             return self[i]
+        
+ """
+ 41. Sort these lists using selection sort
+ a)3,5,4,1,2....1,3,5,4,2....1,2,3,5,4....1,2,3,4,5
+ b)5,4,3,2,1....1,5,4,3,2....1,2,5,4,3....1,2,3,5,4.....1,2,3,4,5
+ c)na
+ 
+ """
+42. Write the selection sort algorithm in pseudocode
+
+        
         
